014f5b550fba46213cc764da2ecf0ae64d4c3b3e
Dat Lv <lvoddat95@yahoo.com>
2023-03-03 12:58:40 +0700

    update

--- ket-sat/assets/js/script.js
+++ ket-sat/assets/js/script.js
@@ -211,6 +211,29 @@ $(function () {
 		});
 	}
 
+	// Lấy thẻ div cần theo dõi
+	const div = document.querySelector('#product-actions-fieldset');
+
+	// Lắng nghe sự kiện cuộn
+	window.addEventListener('scroll', () => {
+		if (isInViewport(div)) {
+			document.body.classList.remove('show-buynow');
+		} else {
+			document.body.classList.add('show-buynow');
+		}
+	});
+
+	// Hàm kiểm tra xem một phần tử có hiển thị trên màn hình hay không
+	function isInViewport(element) {
+		const rect = element.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	}
+
 });
 
 function do_active_slt(elem) {
